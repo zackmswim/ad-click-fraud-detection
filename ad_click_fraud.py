1b00e11913b3ab81722cbbc89ee656403ba26816
zackmswim <zackarymswim@gmail.com>
2025-04-03 13:58:56 -0500

    Updated data analysis script

--- ad_click_fraud.py
+++ ad_click_fraud.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv("ad_click_data.csv")
-print(df.head())
-print(df.info())
-print(df.describe())
+
+df.ffill(inplace=True) #Missing DataApply Forward Fill for time-structured numerical data
+
+df.fillna({
+    'ip_address' : 'Unknown',
+    'user_agent' : 'Unknown User Agent'
+}, inplace=True)
+
+print(df.head())            # shows the first 5 rows
+print(df.info())            # how many columns and rows, data type, if there is missing values
+print(df.describe())        # count, standard deviantion, min/max, 25th, 50th(median), 75th percentile
+print(df.isnull().sum())    # Check for missing values
+
